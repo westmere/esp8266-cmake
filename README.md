1613498f8841e05f995c43e191045ac1f3075c99
vinh <vinh@debian9>
2018-01-20 10:21:52 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# esp8266-cmake
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+-DESP8266_SDK_VARIANT="RTOS"    \
+-DESP8266_SDK_VERSION="2.0.0"   \
+-DESPTOOL=esptool.py  \
+-DESP8266_SDK_BASE=ESP8266_RTOS_SDK \
+-DCMAKE_BUILD_TYPE=Release \
+-G "Eclipse CDT4 - Unix Makefiles" \
+-DCMAKE_ECLIPSE_VERSION=4.6 \
+esp8266-cmake/examples/mqtt_demo
---
+++ cmake/ESP8266Toolchain.cmake
@@ -0,0 +1,93 @@
+if (_IS_TOOLCHAIN_PROCESSED)
+    return()
+endif ()
+set(_IS_TOOLCHAIN_PROCESSED True)
+
+if (NOT DEFINED ESP8266_SDK_VARIANT OR ESP8266_SDK_VARIANT STREQUAL "")
+    set(ESP8266_SDK_VARIANT "RTOS")
+endif()
+
+if (NOT DEFINED ESP8266_SDK_VERSION OR ESP8266_SDK_VERSION STREQUAL "")
+    set(ESP8266_SDK_VERSION "2.0.0")
+endif()
+
+set(CMAKE_SYSTEM_NAME ESP8266)
+set(CMAKE_SYSTEM_VERSION 1)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+
+if(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux")
+    set(USER_HOME $ENV{HOME})
+    set(HOST_EXECUTABLE_PREFIX "")
+    set(ESP8266_ESPTOOL_COM_PORT /dev/ttyUSB0 CACHE STRING "COM port to be used by esptool")
+elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
+    set(USER_HOME $ENV{USERPROFILE})
+    set(HOST_EXECUTABLE_SUFFIX ".exe")
+    set(ESP8266_ESPTOOL_COM_PORT COM1 CACHE STRING "COM port to be used by esptool")
+else()
+    message(FATAL_ERROR Unsupported build platform.)
+endif()
+
+set(ARDUINO_DIR "${USER_HOME}/.arduino15" CACHE PATH "Path to the directory containing the Arduino package files")
+if (ARDUINO_DIR STREQUAL "")
+    message(FATAL_ERROR "ARDUINO_DIR has not been set.")
+endif ()
+
+set(ARDUINO_ESP8266_DIR ${ARDUINO_DIR}/packages/esp8266/hardware/esp8266/2.3.0 CACHE PATH "Path to the directory containing ESP8266 specific arduino files")
+if (ARDUINO_ESP8266_DIR STREQUAL "")
+    message(FATAL_ERROR "ARDUINO_ESP8266_DIR has not been set.")
+endif ()
+
+# file(GLOB_RECURSE ESP8266_XTENSA_C_COMPILERS ${TOOLCHAIN_PREFIX}/bin FOLLOW_SYMLINKS xtensa-lx106-elf-gcc${HOST_EXECUTABLE_SUFFIX})
+# list(GET ESP8266_XTENSA_C_COMPILERS 0 ESP8266_XTENSA_C_COMPILER)
+# file(GLOB_RECURSE ESP8266_XTENSA_CXX_COMPILERS ${TOOLCHAIN_PREFIX}/bin FOLLOW_SYMLINKS xtensa-lx106-elf-g++${HOST_EXECUTABLE_SUFFIX})
+# list(GET ESP8266_XTENSA_CXX_COMPILERS 0 ESP8266_XTENSA_CXX_COMPILER)
+IF(${CMAKE_VERSION} VERSION_LESS 3.6.0)
+    INCLUDE(CMakeForceCompiler)
+    CMAKE_FORCE_C_COMPILER(${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-gcc${TOOL_EXECUTABLE_SUFFIX} GNU)
+    CMAKE_FORCE_CXX_COMPILER(${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-g++${TOOL_EXECUTABLE_SUFFIX} GNU)
+ELSE()
+#     SET(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+    SET(ESP8266_XTENSA_C_COMPILER ${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-gcc${TOOL_EXECUTABLE_SUFFIX})
+    SET(ESP8266_XTENSA_CXX_COMPILER ${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-g++${TOOL_EXECUTABLE_SUFFIX})
+ENDIF()
+file(GLOB_RECURSE ESP8266_ESPTOOLS ${ARDUINO_DIR}/packages/esp8266/tools/esptool FOLLOW_SYMLINKS esptool${HOST_EXECUTABLE_NAME})
+list(GET ESP8266_ESPTOOLS 0 ESP8266_ESPTOOL)
+
+message("Using " ${ESP8266_XTENSA_C_COMPILER} " C compiler.")
+message("Using " ${ESP8266_XTENSA_CXX_COMPILER} " C++ compiler.")
+
+set(CMAKE_C_COMPILER ${ESP8266_XTENSA_C_COMPILER})
+set(CMAKE_CXX_COMPILER ${ESP8266_XTENSA_CXX_COMPILER})
+
+SET(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-objcopy${TOOL_EXECUTABLE_SUFFIX} CACHE INTERNAL "objcopy tool")
+SET(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-objdump${TOOL_EXECUTABLE_SUFFIX} CACHE INTERNAL "objdump tool")
+SET(CMAKE_SIZE ${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-size${TOOL_EXECUTABLE_SUFFIX} CACHE INTERNAL "size tool")
+SET(CMAKE_DEBUGER ${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-gdb${TOOL_EXECUTABLE_SUFFIX} CACHE INTERNAL "debuger")
+SET(CMAKE_CPPFILT ${TOOLCHAIN_PREFIX}/bin/xtensa-lx106-elf-c++filt${TOOL_EXECUTABLE_SUFFIX} CACHE INTERNAL "C++filt")
+
+# set(CMAKE_C_FLAGS "-w -Os -g -std=gnu99 -Wpointer-arith -Wno-implicit-function-declaration -Wundef -pipe -D__ets__ -DICACHE_FLASH -fno-inline-functions -ffunction-sections -nostdlib -mlongcalls -mtext-section-literals -falign-functions=4 -fdata-sections" CACHE STRING "C compiler flags" FORCE)
+# set(CMAKE_CXX_FLAGS "-w -Os -g -D__ets__ -DICACHE_FLASH -mlongcalls -mtext-section-literals -fno-exceptions -fno-rtti -falign-functions=4 -std=c++11 -MMD -ffunction-sections -fdata-sections" CACHE STRING "CXX compiler flags" FORCE)
+# set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -Wl,--no-check-sections -Wl,-static -Wl,--gc-sections -L${ARDUINO_ESP8266_DIR}/tools/sdk/ld -Teagle.flash.${ESP8266_FLASH_SIZE}.ld -u call_user_start -Wl,-wrap,system_restart_local -Wl,-wrap,register_chipv6_phy" CACHE STRING "linker flags" FORCE)
+# set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -Wl,--no-check-sections -Wl,-static -Wl,--gc-sections -u call_user_start -Wl,-wrap,system_restart_local -Wl,-wrap,register_chipv6_phy" CACHE STRING "linker flags" FORCE)
+
+set(CCFLAGS  "-g -Wpointer-arith -Wundef -Werror -Wl,-EL -DICACHE_FLASH -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -ffunction-sections -fdata-sections")
+set(LDFLAGS "-nostdlib -Wl,--no-check-sections -Wl,-static -Wl,--gc-sections -u call_user_start")
+SET(CMAKE_C_FLAGS "-Os ${CCFLAGS}" CACHE INTERNAL "c compiler flags release")
+SET(CMAKE_CXX_FLAGS "-Os ${CCFLAGS}" CACHE INTERNAL "cxx compiler flags release")
+SET(CMAKE_ASM_FLAGS "" CACHE INTERNAL "asm compiler flags release")
+SET(CMAKE_EXE_LINKER_FLAGS "${LDFLAGS}" CACHE INTERNAL "linker flags release")
+
+SET(CMAKE_C_FLAGS_DEBUG "-O0 -ggdb" CACHE INTERNAL "c compiler flags debug")
+SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -ggdb" CACHE INTERNAL "cxx compiler flags debug")
+SET(CMAKE_ASM_FLAGS_DEBUG "-g" CACHE INTERNAL "asm compiler flags debug")
+SET(CMAKE_EXE_LINKER_FLAGS_DEBUG "" CACHE INTERNAL "linker flags debug")
+
+SET(CMAKE_C_FLAGS_RELEASE "-Os" CACHE INTERNAL "c compiler flags release")
+SET(CMAKE_CXX_FLAGS_RELEASE "-Os" CACHE INTERNAL "cxx compiler flags release")
+SET(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "asm compiler flags release")
+SET(CMAKE_EXE_LINKER_FLAGS_RELEASE "" CACHE INTERNAL "linker flags release")
+
+set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_C_COMPILER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> -o <TARGET> -Wl,--start-group <OBJECTS> <LINK_LIBRARIES> -Wl,--end-group" CACHE STRING "C linker invocation")
+set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_CXX_COMPILER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> -o <TARGET> -Wl,--start-group <OBJECTS> <LINK_LIBRARIES> -Wl,--end-group" CACHE STRING "CXX linker invocation")
+
