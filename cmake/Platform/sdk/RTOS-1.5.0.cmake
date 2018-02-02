a972b6114c7ee961aa3e41ccb10211530e254b62
vinh <vinh@debian9>
2018-02-02 14:40:29 +0700

    ...

--- cmake/Platform/sdk/RTOS-1.5.0.cmake
+++ cmake/Platform/sdk/RTOS-1.5.0.cmake
@@ -128,6 +128,22 @@ else()
     set(BIN_NAME "eagle.irom0text.bin")
 endif()
 
+set(ESP8266_RTOS_SDK_INCLUDE
+    "${ESP8266_SDK_BASE}/include"
+    "${ESP8266_SDK_BASE}/driver_lib/include"
+    "${ESP8266_SDK_BASE}/include/json"
+    "${ESP8266_SDK_BASE}/extra_include"
+    "${ESP8266_SDK_BASE}/include/espressif"
+    "${ESP8266_SDK_BASE}/include/lwip"
+    "${ESP8266_SDK_BASE}/include/lwip/ipv4"
+    "${ESP8266_SDK_BASE}/include/lwip/ipv6"
+    "${ESP8266_SDK_BASE}/include/nopoll"
+    "${ESP8266_SDK_BASE}/include/spiffs"
+    "${ESP8266_SDK_BASE}/include/ssl"
+    "${ESP8266_SDK_BASE}/include/json"
+    "${ESP8266_SDK_BASE}/include/freertos"
+)
+
 function(esp8266_add_library TARGET)
 #     cmake_parse_arguments(
 #         PARSED_ARGS # prefix of output variables
@@ -142,21 +158,7 @@ function(esp8266_add_library TARGET)
     )
     add_library(${TARGET} STATIC ${driver_lib} ${ARGN})
     
-#     target_include_directories(${TARGET} PUBLIC
-#         "${ESP8266_SDK_BASE}/include"
-#         "${ESP8266_SDK_BASE}/driver_lib/include"
-#         "${ESP8266_SDK_BASE}/include/json"
-#         "${ESP8266_SDK_BASE}/extra_include"
-#         "${ESP8266_SDK_BASE}/include/espressif"
-#         "${ESP8266_SDK_BASE}/include/lwip"
-#         "${ESP8266_SDK_BASE}/include/lwip/ipv4"
-#         "${ESP8266_SDK_BASE}/include/lwip/ipv6"
-#         "${ESP8266_SDK_BASE}/include/nopoll"
-#         "${ESP8266_SDK_BASE}/include/spiffs"
-#         "${ESP8266_SDK_BASE}/include/ssl"
-#         "${ESP8266_SDK_BASE}/include/json"
-#         "${ESP8266_SDK_BASE}/include/freertos"
-#     )
+    target_include_directories(${TARGET} PRIVATE ${ESP8266_RTOS_SDK_INCLUDE})
     
 #     get_property(dirs TARGET ${TARGET} PROPERTY INCLUDE_DIRECTORIES)
 #     foreach(dir ${dirs})
@@ -312,23 +314,3 @@ function(esp8266_add_firmware FIRMWARE TARGET)
     )
     add_dependencies(${FIRMWARE} ${TARGET})
 endfunction()
-
-if (_SDK_INCLUE_PROCESSED)
-    return()
-endif ()
-include_directories(${TARGET}
-    "${ESP8266_SDK_BASE}/include"
-    "${ESP8266_SDK_BASE}/driver_lib/include"
-    "${ESP8266_SDK_BASE}/include/json"
-    "${ESP8266_SDK_BASE}/extra_include"
-    "${ESP8266_SDK_BASE}/include/espressif"
-    "${ESP8266_SDK_BASE}/include/lwip"
-    "${ESP8266_SDK_BASE}/include/lwip/ipv4"
-    "${ESP8266_SDK_BASE}/include/lwip/ipv6"
-    "${ESP8266_SDK_BASE}/include/nopoll"
-    "${ESP8266_SDK_BASE}/include/spiffs"
-    "${ESP8266_SDK_BASE}/include/ssl"
-    "${ESP8266_SDK_BASE}/include/json"
-    "${ESP8266_SDK_BASE}/include/freertos"
-)
-set(_SDK_INCLUE_PROCESSED True)
