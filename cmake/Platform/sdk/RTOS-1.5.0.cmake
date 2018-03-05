73d2a9b3d41c93080a85f1aefeca325b8d4bf46d
vinh <vinh@debian9>
2018-03-05 11:39:38 +0700

    ...

--- cmake/Platform/sdk/RTOS-1.5.0.cmake
+++ cmake/Platform/sdk/RTOS-1.5.0.cmake
@@ -254,7 +254,6 @@ function(esp8266_add_firmware FIRMWARE TARGET)
                         COMMAND rm -f eagle.S eagle.dump
                         COMMAND ${CMAKE_OBJDUMP} -x -s ${TARGET} > eagle.dump
                         COMMAND ${CMAKE_OBJDUMP} -S ${TARGET} > eagle.S
-                        COMMAND ${CMAKE_COMMAND} -E copy ${ESP8266_SDK_BASE}/bin/${BOOT_NAME} ${CMAKE_BINARY_DIR}/${BOOT_NAME}
                         COMMAND ${CMAKE_OBJCOPY} --only-section .text -O binary ${TARGET} eagle.app.v6.text.bin
                         COMMAND ${CMAKE_OBJCOPY} --only-section .data -O binary ${TARGET} eagle.app.v6.data.bin
                         COMMAND ${CMAKE_OBJCOPY} --only-section .rodata -O binary ${TARGET} eagle.app.v6.rodata.bin
@@ -285,6 +284,7 @@ function(esp8266_add_firmware FIRMWARE TARGET)
                         ALL
                         VERBATIM
                         DEPENDS RTOS_SDK_FIRMWARE0
+                        COMMAND ${CMAKE_COMMAND} -E copy ${ESP8266_SDK_BASE}/bin/${BOOT_NAME} ${CMAKE_BINARY_DIR}/firmware/${BOOT_NAME}
                         COMMAND ${CMAKE_COMMAND} -E rename eagle.app.flash.bin ${CMAKE_BINARY_DIR}/firmware/${BIN_NAME}
                         COMMAND ${CMAKE_COMMAND} -E remove eagle.app.v6.*
                         COMMAND @${CMAKE_COMMAND} -E echo ${msg}
